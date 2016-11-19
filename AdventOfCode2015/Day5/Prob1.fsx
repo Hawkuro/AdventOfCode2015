12a83e2e92fd7572fe363bcbb62a295bf21ed97c
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-19 16:09:10 +0000

    Day 5 complete

--- AdventOfCode2015/Day5/Prob1.fsx
+++ AdventOfCode2015/Day5/Prob1.fsx
@@ -1,1 +1,51 @@
-﻿
+﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day5\input.txt") |> Seq.toList;
+
+let vowels = "aeiou" |> Seq.toList
+
+let (|Vowel|Consonant|) (c : char) = if Seq.exists ((=) c) vowels then Vowel else Consonant
+
+let countVowels str = 
+    let isVowel c =
+        match c with
+        | Vowel -> true
+        | Consonant -> false
+    str |> Seq.filter isVowel |> Seq.length
+//    let rec countVowels' n str =
+//        match Seq.length str with
+//        | 0 -> n
+//        | _ -> 
+//            match List.head str with
+//            | Vowel -> countVowels' (n+1) (List.tail str)
+//            | Consonant -> countVowels' n (List.tail str)
+//    countVowels' 0 str
+
+let (|ThreeVowels|_|) str = if countVowels str >= 3 then Some ThreeVowels else None
+
+
+let hasDoubleLetter str = Seq.pairwise str |> Seq.map (fun (a,b) -> a=b) |> Seq.reduce (||) 
+
+let (|DoubleLetter|_|) str = if hasDoubleLetter str then Some DoubleLetter else None
+
+
+let badPairs = ["ab";"cd";"pq";"xy"]
+
+let (|BadPair|_|) str = if Seq.exists ((=) str) badPairs then Some BadPair else None
+
+let isBadPair str = 
+    match str with
+    | BadPair -> true
+    | _ -> false
+
+let toString : char seq -> string = Seq.map string >> String.concat ""
+
+let hasBadPair (str : char list) = Seq.pairwise str |> Seq.map (fun (a,b) -> [a;b] |> toString) |> Seq.exists isBadPair
+
+let (|HasBadPair|HasNoBadPairs|) str = if hasBadPair str then HasBadPair else HasNoBadPairs
+
+
+let isNice (str : string) = 
+    match Seq.toList str with
+    | ThreeVowels & DoubleLetter & HasNoBadPairs -> true
+    | _ -> false
+
+input |> Seq.filter isNice |> Seq.length |> printfn "%A"
---
+++ AdventOfCode2015/Day5/Prob2.fsx
@@ -0,0 +1,43 @@
+﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day5\input.txt") |> Seq.toList;
+let toString : char seq -> string = Seq.map string >> String.concat ""
+
+
+let checkRemovedPairs (pred : char seq -> char*char -> bool) (str : char seq) = 
+    let rec checkRemovedPairs' (head : char seq) (pair : char*char) (tail:char seq) (result : bool list) =
+        match (Seq.toList head,pair,Seq.toList tail) with
+        | (_,_,[]) -> (pred head pair)::result
+        | (h,(f,s),th::t) -> checkRemovedPairs' (h @ [f]) (s,th) t ((pred (h@'-'::th::t) pair)::result)
+    match Seq.toList str with
+    | a::b::t -> checkRemovedPairs' [] (a,b) t []
+    | _ -> []
+
+let containsPair str pair =
+    Seq.pairwise str |> Seq.exists ((=) pair)
+
+let (|HasDoublePair|_|) str = if checkRemovedPairs containsPair str |> Seq.reduce (||) then Some HasDoublePair else None
+
+ 
+let tripletwise (l :  seq<char>) = 
+    let rec tripletwise' (l : char list) o = 
+        match l with 
+        | a::b::c::tail -> tripletwise' (b::c::tail) ((toString [a;b;c])::o)
+        | _ -> o
+    tripletwise' (Seq.toList l) []
+
+let isNiceTriplet (trip : string) = 
+    match Seq.toList trip with
+    | [a;_;b] when a=b -> true
+    | _ -> false
+
+let (|HasNiceTriplet|_|) (str: char list) =
+    if (str |> tripletwise |> Seq.exists isNiceTriplet)
+    then Some HasNiceTriplet
+    else None
+
+
+let isNice (str : string) = 
+    match Seq.toList str with
+    | HasNiceTriplet & HasDoublePair -> true
+    | _ -> false
+
+input |> Seq.filter isNice |> Seq.length |> printfn "%A"

4125f9e1ec8156044ebf8a71a5a2761f26cdd77d
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-20 13:59:08 +0000

    Some mutability for caching purposes, but it got done

--- AdventOfCode2015/Day 7/Prob1.fsx
+++ AdventOfCode2015/Day 7/Prob1.fsx
@@ -1,61 +1,45 @@
-﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day 7\input.txt");
+﻿open System.Collections.Generic
+
+let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day 7\input.txt");
 
 
-let calculate (opeMap : Map<string,_>) getValue = opeMap.[getValue](opeMap) 
 
 let (|Integer|_|) (str: string) =
    let mutable intvalue = uint16 0
    if System.UInt16.TryParse(str, &intvalue) then Some(intvalue)
    else None
 
-let (|Not|_|) (str: string) =
+let readInputString (str :string) = 
     match (str.Split ' ') with
-    | [|"NOT";Integer inp;"->";outp|] -> Some(outp,(fun opeMap -> ~~~ inp))
-    | [|"NOT";inp;"->";outp|] -> Some(outp,(fun opeMap -> ~~~ (calculate opeMap inp)))
-    | _ -> None
-
-let (|Or|_|) (str: string) =
-    match (str.Split ' ') with
-    | [|Integer a;"OR";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a ||| b))
-    | [|a;"OR";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) ||| b))
-    | [|Integer a;"OR";b;"->";outp|] -> Some(outp,(fun opeMap -> a ||| (calculate opeMap b)))
-    | [|a;"OR";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) ||| (calculate opeMap b)))
-    | _ -> None
-
-let (|And|_|) (str: string) =
-    match (str.Split ' ') with
-    | [|Integer a;"AND";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a &&& b))
-    | [|a;"AND";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) &&& b))
-    | [|Integer a;"AND";b;"->";outp|] -> Some(outp,(fun opeMap -> a &&& (calculate opeMap b)))
-    | [|a;"AND";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) &&& (calculate opeMap b)))
-    | _ -> None
-
-let (|RShift|_|) (str: string) =
-    match (str.Split ' ') with
-    | [|Integer a;"RSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a >>> int32 b))
-    | [|a;"RSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) >>> int32 b))
-    | [|Integer a;"RSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> a >>> int32 (calculate opeMap b)))
-    | [|a;"RSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) >>> int32 (calculate opeMap b)))
-    | _ -> None
-
-let (|LShift|_|) (str: string) =
-    match (str.Split ' ') with
-    | [|Integer a;"LSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a <<< int32 b))
-    | [|a;"LSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) <<< int32 b))
-    | [|Integer a;"LSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> a <<< int32 (calculate opeMap b)))
-    | [|a;"LSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) <<< int32 (calculate opeMap b)))
-    | _ -> None
-
-let (|Input|_|) (str: string) =
-    match (str.Split ' ') with
-    | [|Integer i;"->";outp|] -> Some(outp,(fun opeMap -> i))
-    | [|inp;"->";outp|] -> Some(outp,(fun opeMap -> calculate opeMap inp))
-    | _ -> None
-
-let readInputString str = 
-    match str with
-    |Not op|Or op|And op|RShift op|LShift op|Input op -> op
+    | [|a;"AND";b;"->";outp|] -> (outp,((&&&),[a;b]))
+    | [|a;"OR";b;"->";outp|] -> (outp,((|||),[a;b]))
+    | [|a;"RSHIFT";b;"->";outp|] -> (outp,( (fun x y -> x >>> int32 y) ,[a;b]))
+    | [|a;"LSHIFT";b;"->";outp|] -> (outp,( (fun x y -> x <<< int32 y) ,[a;b]))
+    | [|"NOT";a;"->";outp|] -> (outp,((fun x y -> ~~~ x),[a]))
+    | [|inp;"->";outp|] -> (outp,((fun x y -> x),[inp]))
     | _ -> failwith ("unrecognized command: "+str)
 
-let opMap = input |> Seq.map readInputString |> Map.ofSeq
 
+let opMap = input |> Seq.map readInputString |> Map.ofSeq
+let mutable Values = new Dictionary<string,uint16>()
+
+let rec calculate (values : IDictionary<string,uint16>) (opeMap : Map<string,((uint16 -> uint16 -> uint16)*string list)>) getValue = 
+    let rec calculate' values (opeMap : Map<string,((uint16 -> uint16 -> uint16)*string list)>) getValue = 
+        printfn "%s" getValue
+        let (|Literal|_|) str = 
+            match str with
+            | Integer i -> Some i
+            | s -> Some (calculate values opeMap s)
+            | _ -> failwith ("Parse error: "+str)
+        let command = opeMap.[getValue]
+        let ans =
+            match command with
+            | (comm,[Literal a;Literal b]) -> comm a b
+            | (comm,[Literal a]) -> comm a a
+            | _ -> failwith ("Failed to execute command for "+getValue)
+        values.Add (getValue,ans)
+        printfn "%s" (getValue+" = "+ans.ToString())
+        ans
+    if values.ContainsKey getValue then values.[getValue] else calculate' values opeMap getValue
+
+calculate (Values) opMap "a" |> printfn "%A"

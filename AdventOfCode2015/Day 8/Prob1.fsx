41ba820586edec3a379c34b9c71c1d533a4737a4
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-20 21:41:24 +0000

    Day 8 done

---
+++ AdventOfCode2015/Day 8/Prob1.fsx
@@ -0,0 +1,12 @@
+﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day 8\input.txt");
+
+let calcStringDiff str = 
+    let rec calcStringDiff' str n =
+        match str with
+        | [] -> n
+        | '\\'::'x'::_::_::tail -> calcStringDiff' tail (n+3)
+        | '\\'::_::tail -> calcStringDiff' tail (n+1)
+        | _::tail -> calcStringDiff' tail n
+    calcStringDiff' (Seq.toList str) 2
+
+input |> Seq.map calcStringDiff |> Seq.sum |> printfn "%A"
---
+++ AdventOfCode2015/Day 8/Prob2.fsx
@@ -0,0 +1,10 @@
+﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day 8\input.txt");
+
+let calcStringDiff str = 
+    let calcStringDiff' c =
+        match c with
+        | '\"' | '\\' -> 2
+        | _ -> 1
+    str |> Seq.map calcStringDiff' |> Seq.sum |> (+) 2 |> (-) (Seq.length str)
+
+input |> Seq.map calcStringDiff |> Seq.sum |> printfn "%A"

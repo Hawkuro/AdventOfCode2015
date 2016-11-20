e03d27a1430afbd3873925e01527626cdd0043a3
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-20 01:25:16 +0000

    Day 7 prob 1 prototype, doens't work. Day 6 complete

---
+++ AdventOfCode2015/Day 6/Prob1.fsx
@@ -0,0 +1,28 @@
+﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day 6\input.txt");
+
+let mutable baseGrid = List.toArray [for i in 0..999 -> List.toArray [for i in 0..999 -> ref false]]
+
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = 0
+   if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let stringToTuple (str : string) = 
+    match (str.Split ',') with
+    | [|Integer x;Integer y|] -> (x,y)
+    | _ -> (-1,-1)
+
+let turnOn (x1,y1) (x2,y2) = [for i in x1..x2->i] |> Seq.collect (fun i -> [for j in y1..y2 -> (i,j)]) |> Seq.iter (fun (i,j) -> baseGrid.[i].[j] := true)
+
+let turnOff (x1,y1) (x2,y2) = [for i in x1..x2->i] |> Seq.collect (fun i -> [for j in y1..y2 -> (i,j)]) |> Seq.iter (fun (i,j) -> baseGrid.[i].[j] := false)
+
+let toggle (x1,y1) (x2,y2) = [for i in x1..x2->i] |> Seq.collect (fun i -> [for j in y1..y2 -> (i,j)]) |> Seq.iter (fun (i,j) -> baseGrid.[i].[j] := not !baseGrid.[i].[j])
+
+let chooseCommand (str : string) =
+    match (str.Split ' ') with
+    | [|"turn";"on";coords1;"through";coords2|] -> turnOn (stringToTuple coords1) (stringToTuple coords2)
+    | [|"turn";"off";coords1;"through";coords2|] -> turnOff (stringToTuple coords1) (stringToTuple coords2)
+    | [|"toggle";coords1;"through";coords2|] -> toggle (stringToTuple coords1) (stringToTuple coords2)
+
+input |> Seq.iter chooseCommand
+baseGrid |> Seq.map (Seq.map (fun a-> !a)) |> Seq.collect (fun a-> a) |> Seq.filter ((=) true) |> Seq.length |> printfn "%A"
---
+++ AdventOfCode2015/Day 7/Prob1.fsx
@@ -0,0 +1,61 @@
+﻿let input = System.IO.File.ReadAllLines(@"C:\Users\hawku\Documents\Visual Studio 2015\Projects\AdventOfCode2015\AdventOfCode2015\Day 7\input.txt");
+
+
+let calculate (opeMap : Map<string,_>) getValue = opeMap.[getValue](opeMap) 
+
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = uint16 0
+   if System.UInt16.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let (|Not|_|) (str: string) =
+    match (str.Split ' ') with
+    | [|"NOT";Integer inp;"->";outp|] -> Some(outp,(fun opeMap -> ~~~ inp))
+    | [|"NOT";inp;"->";outp|] -> Some(outp,(fun opeMap -> ~~~ (calculate opeMap inp)))
+    | _ -> None
+
+let (|Or|_|) (str: string) =
+    match (str.Split ' ') with
+    | [|Integer a;"OR";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a ||| b))
+    | [|a;"OR";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) ||| b))
+    | [|Integer a;"OR";b;"->";outp|] -> Some(outp,(fun opeMap -> a ||| (calculate opeMap b)))
+    | [|a;"OR";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) ||| (calculate opeMap b)))
+    | _ -> None
+
+let (|And|_|) (str: string) =
+    match (str.Split ' ') with
+    | [|Integer a;"AND";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a &&& b))
+    | [|a;"AND";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) &&& b))
+    | [|Integer a;"AND";b;"->";outp|] -> Some(outp,(fun opeMap -> a &&& (calculate opeMap b)))
+    | [|a;"AND";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) &&& (calculate opeMap b)))
+    | _ -> None
+
+let (|RShift|_|) (str: string) =
+    match (str.Split ' ') with
+    | [|Integer a;"RSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a >>> int32 b))
+    | [|a;"RSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) >>> int32 b))
+    | [|Integer a;"RSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> a >>> int32 (calculate opeMap b)))
+    | [|a;"RSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) >>> int32 (calculate opeMap b)))
+    | _ -> None
+
+let (|LShift|_|) (str: string) =
+    match (str.Split ' ') with
+    | [|Integer a;"LSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> a <<< int32 b))
+    | [|a;"LSHIFT";Integer b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) <<< int32 b))
+    | [|Integer a;"LSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> a <<< int32 (calculate opeMap b)))
+    | [|a;"LSHIFT";b;"->";outp|] -> Some(outp,(fun opeMap -> (calculate opeMap a) <<< int32 (calculate opeMap b)))
+    | _ -> None
+
+let (|Input|_|) (str: string) =
+    match (str.Split ' ') with
+    | [|Integer i;"->";outp|] -> Some(outp,(fun opeMap -> i))
+    | [|inp;"->";outp|] -> Some(outp,(fun opeMap -> calculate opeMap inp))
+    | _ -> None
+
+let readInputString str = 
+    match str with
+    |Not op|Or op|And op|RShift op|LShift op|Input op -> op
+    | _ -> failwith ("unrecognized command: "+str)
+
+let opMap = input |> Seq.map readInputString |> Map.ofSeq
+

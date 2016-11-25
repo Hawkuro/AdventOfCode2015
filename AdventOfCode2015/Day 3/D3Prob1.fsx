7b5181cc5612c7a2953b2f807cb728804283e0f8
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-25 00:29:26 +0000

    Added Day 3 problem 1

---
+++ AdventOfCode2015/Day 3/D3Prob1.fsx
@@ -0,0 +1,17 @@
+﻿let input = System.IO.File.ReadAllText(__SOURCE_DIRECTORY__+ @"\D3input.txt")
+
+let nextHouse (x,y) c = 
+    match c with
+    | '^' -> (x,y+1)
+    | '>' -> (x+1,y)
+    | 'v' -> (x,y-1)
+    | '<' -> (x-1,y)
+    | _ -> (x,y)
+    
+let visitHouses ((visited : Set<_>),location) c = 
+    let nh = nextHouse location c
+    (visited.Add(nh),nh)
+
+input |> Seq.toList |> List.fold visitHouses (Set.empty.Add((0,0)),(0,0)) |> fst 
+    |> Set.count
+    |> printfn "%A"

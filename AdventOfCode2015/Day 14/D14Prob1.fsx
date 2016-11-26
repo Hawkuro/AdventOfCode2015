5a4915c025d2c0c99e8272a6abe2236637f6f635
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-26 13:20:04 +0000

    Day 14 prob 1 successful

---
+++ AdventOfCode2015/Day 14/D14Prob1.fsx
@@ -0,0 +1,24 @@
+﻿let input = System.IO.File.ReadAllLines(__SOURCE_DIRECTORY__+ @"\D14input.txt");
+let inputTime = 2503
+
+type Reindeer = {name : string; speed: int; endurance: int; restTime: int }
+
+let (|Integer|_|) (str: string) =
+   let mutable intvalue = int 0
+   if System.Int32.TryParse(str, &intvalue) then Some(intvalue)
+   else None
+
+let parseReindeerString (str: string) = 
+    match (str.Split [|' ';'.'|]) with
+    | [|name;"can";"fly";Integer speed;"km/s";"for";Integer endurance;_;_;_;_;_;_;Integer restTime;_;_|] ->
+        {name = name;speed = speed;endurance = endurance;restTime=restTime}
+    | _ -> failwith ("Could not parse string: "+str)
+
+let calculateReindeerDistance time (reindeer:Reindeer) =
+    let calculateRemainderDistance remainder =
+        reindeer.speed*(min reindeer.endurance remainder)
+    let spurtDist = reindeer.speed*reindeer.endurance
+    let spurtLength = reindeer.endurance + reindeer.restTime
+    spurtDist*(time / spurtLength) + calculateRemainderDistance (time % spurtLength)
+
+input |> Seq.map parseReindeerString |> Seq.map (calculateReindeerDistance inputTime) |> Seq.max |> printfn "%A"

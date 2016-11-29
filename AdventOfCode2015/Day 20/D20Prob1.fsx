fc1a74c290a10523261f0343f72a705a037a62c8
Haukur Óskar Þorgeirsson <hawkuro@gmail.com>
2016-11-29 23:32:49 +0000

    Day 20 prob 1 done

---
+++ AdventOfCode2015/Day 20/D20Prob1.fsx
@@ -0,0 +1,13 @@
+﻿let input = 29000000
+
+open System
+
+let listOfDivisors (n:int) = 
+    let sqrt = n |> (float) |> Math.Sqrt |> Math.Floor |> (int)
+    [for i in 1..sqrt -> i]
+    |> Seq.filter (fun i -> n % i = 0)
+    |> Seq.map (fun i -> [i;n/i])
+    |> Seq.collect id
+    |> Seq.distinct
+
+Seq.initInfinite (fun i -> listOfDivisors i |> Seq.sum |> (*) 10) |> Seq.findIndex (fun i -> i >= input) |> printfn "%A"
